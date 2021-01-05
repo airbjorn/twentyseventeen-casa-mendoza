acf08a4ea7ec515a57e42568a2ced03c5c433a52
Björn Gerhart <airbjorn@users.noreply.github.com>
2021-01-05 09:07:01 +0100

    Update functions.php
    simplify child theme

--- functions.php
+++ functions.php
@@ -1,26 +1,12 @@
 <?php
-// Fix Privacy Hole: hide internal wp information for anybody.
-// See: c't 23/2020 p 28 in the context of allowed JSON-API calls
-add_filter( 'rest_authentication_errors', function( $result ) { 
-  if ( ! empty( $result ) ) { 
-    return $result; 
-  } 
-  if ( ! is_user_logged_in() ) { 
-    return new WP_Error( '401', 'not allowed.', array('status' => 401) ); 
-  } 
-  return $result;
-});
+function mychildtheme_enqueue_styles() {
+    $parent_style = 'parent-style';
 
-// Enqueue parent theme styles and child theme stylesheet
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style )
+    );
 }
-// Enqueue child them js file
-add_action( 'wp_enqueue_scripts', 'child_theme_js' );
-function child_theme_js() {
-  wp_enqueue_script( 'child-theme-js' , get_stylesheet_directory_uri() . '/child-theme-js.js' , array( 'twentyseventeen-global' ) , false , true );
-}
-
+add_action( 'wp_enqueue_scripts', 'mychildtheme_enqueue_styles' );
 ?>
